f8be2f051e70035dc4aae7e897e7c71b77e21181
adethnunez <bernadethnunez005@gmail.com>
2023-05-31 15:38:58 +0800

    slider

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,23 @@
 const headerNav = document.querySelector('.header__nav');
+const contactNav = document.querySelector('.contact__nav');
+const toggleContact = document.querySelector('.toggle__contact');
 const toggleMenu = document.querySelector('.toggle__menu');
 toggleMenu.addEventListener('click', ()=>{
     headerNav.classList.toggle('open');
     toggleMenu.classList.toggle('open');
+    contactNav.classList.remove('open');
+    toggleContact.classList.remove('open');
 });
 
-const contactNav = document.querySelector('.contact__nav');
-const toggleContact = document.querySelector('.toggle__contact');
+
 toggleContact.addEventListener('click', ()=>{
     contactNav.classList.toggle('open');
     toggleContact.classList.toggle('open');
+    headerNav.classList.remove('open');
+    toggleMenu.classList.remove('open');
 });
 
+//scroll
 const nav = document.querySelectorAll(".header__nav ul li a");
 nav.forEach((baba) => {
   baba.addEventListener("click", () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
         dark: "hsl(208.7,51.11%,17.65%)",
         light: "hsl(208.7,51.11%,17.65%)",
         primary: "hsl(175.96,98.89%,35.29%)",
+        itim:'#323232',
+        puti:'#ffffff',
+        grey: '#f6f6f6',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      backgroundImage:{
+        'slider1': 'url("./img/slider1.jpg")',
+        'slider2': 'url("./img/slider2.jpg")',
+        'slider3': 'url("./img/slider3.jpg")',
       }
     },
   },
